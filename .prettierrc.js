9ab14dc27ca4760a8c19b22eaad57f275b9aeaca
Paterson <86971279+paterson1@users.noreply.github.com>
2022-06-09 22:09:07 -0300

    feat: :memo: Follow documentation

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    useTabs: false,
+    semi: true,
+    singleQuote: true, // default=false
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'none', // default='es5'
+    bracketSpacing: true,
+    jsxBracketSameLine: true, // default=false
+    arrowParens: 'always',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'css',
+    endOfLine: 'lf'
+};
